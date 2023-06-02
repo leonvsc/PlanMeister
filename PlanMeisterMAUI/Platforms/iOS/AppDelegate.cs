28b93ffac7a0cb4c2498482ca77a04fbcc0bb5e9
Leon <leonvsc@users.noreply.github.com>
2023-06-02 13:58:19 +0200

    Feat: Added MAUI template, Razor Library class template and a empty solution to the project

---
+++ PlanMeisterMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PlanMeisterMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
